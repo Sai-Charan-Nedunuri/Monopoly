0e487dd23e71269ab76d4c74a92edb32c50f68aa
NEDUNURI SAI CHARAN <Sai-Charan-Nedunuri@users.noreply.github.com>
2021-03-13 15:03:18 +0530

    .

---
+++ src/Space.java
@@ -0,0 +1,51 @@
+package monopolyGame;
+
+public  class Space implements Property {
+
+	public Space(String string, int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void affect(Player p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Player getOwner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setOwner(Player p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
